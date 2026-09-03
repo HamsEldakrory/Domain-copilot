15d2a627e4b8236a85e088a26872c63a9dd77a61
hamseldakrory <hamseldakrory14@gmail.com>
2026-09-04 02:44:37 +0300

    feat: update App component with new routes and enhance index.css for improved styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
